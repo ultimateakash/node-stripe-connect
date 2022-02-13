875d0f3b087e42153a068f14f9dca67dcdd734aa
Akash Kumar Verma <akash@yallafelsekka.com>
2022-02-13 23:56:44 +0530

    Update Snapshot

--- config/stripe.js
+++ config/stripe.js
@@ -2,5 +2,5 @@
     clientId: process.env.STRIPE_CLIENT_ID,  // Client ID: https://dashboard.stripe.com/account/applications/settings
     secretKey: process.env.STRIPE_SECRET_KEY,   // Secret KEY: https://dashboard.stripe.com/account/apikeys
     redirectUri: process.env.STRIPE_REDIRECT_URI, // Redirect Uri https://dashboard.stripe.com/account/applications/settings
-	authorizationUri: process.env.STRIPE_AUTHORIZATION_URI
+	authorizationUri: 'https://connect.stripe.com/oauth/authorize'
 }
